ba2dffbc795572d250eb1ec0a7906d2b824837d9
Griffin Mcarthur <Griffinmcarthur1@gmail.com>
2022-11-13 15:31:51 -0700

    commit tree eadc1c65047ce2a78697b49b46aebe791a130504

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+const employee = {}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+return Object.assign({}, employee, { [key]: value });
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+
+  function deleteFromEmployeeByKey(employee, key) {
+    const newemployee =  Object.assign({}, employee);
+    delete newemployee[key];
+    return newemployee;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(driver, key) {
+    Object.assign({}, employee);
+    delete employee[key]; 
+    return employee;
+    }
